d1c813f874fc54be8b3e310f301d60fc89c991fd
ran <ran@aroussi.com>
2016-08-25 15:57:23 +0300

    fixed docs sidebar links

--- docs/source/_static/custom.js
+++ docs/source/_static/custom.js
@@ -17,13 +17,13 @@ window.onload = function() {
 
     // decorate sidebar
     $('.wy-side-scroll').after('<div id="side-ext"></div>');
-    $('#side-ext').append('<a target="_new" href="https://github.com/ranaroussi/qtpylib/raw/master/docs/build/epub/QTPyLib.epub" class="fa fa-book">ePub</a>');
+    $('#side-ext').append('<a target="_new" download="qtpylib-docs.epub" href="https://github.com/ranaroussi/qtpylib/raw/master/docs/build/epub/QTPyLib.epub" class="fa fa-book">ePub</a>');
     $('#side-ext').append(' &nbsp;|&nbsp; ');
-    $('#side-ext').append('<a target="_new" href="https://github.com/ranaroussi/qtpylib/raw/master/docs/build/html.zip" class="fa fa-file-archive-o">Zip</a>');
+    $('#side-ext').append('<a target="_new" download="qtpylib-docs.zip" href="https://github.com/ranaroussi/qtpylib/raw/master/docs/build/html.zip" class="fa fa-file-archive-o">Zip</a>');
     $('#side-ext').append(' &nbsp; || &nbsp; ');
-    $('#side-ext').append('<a target="_new" href="https://github.com/ranaroussi/qtpylib" class="fa fa-github">GitHub</a>');
+    $('#side-ext').append('<a target="github" href="https://github.com/ranaroussi/qtpylib" class="fa fa-github">GitHub</a>');
     $('#side-ext').append(' &nbsp;|&nbsp; ');
-    $('#side-ext').append('<a target="_new" href="https://twitter.com/aroussi" class="fa fa-twitter">Twitter</a>');
+    $('#side-ext').append('<a target="twitter" href="https://twitter.com/aroussi" class="fa fa-twitter">Twitter</a>');
   }
 
 }
