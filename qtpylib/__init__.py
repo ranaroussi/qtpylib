d6da748d904802e82257cb81cd37909d8027c0da
ran <ran@aroussi.com>
2016-10-25 13:16:42 +0300

    updated version #

--- qtpylib/__init__.py
+++ qtpylib/__init__.py
@@ -19,7 +19,7 @@
 # limitations under the License.
 #
 
-__version__ = '1.4.95a'
+__version__ = '1.4.96a'
 __author__ = 'Ran Aroussi'
 
 from . import *
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='QTPyLib',
-    version='1.4.95',
+    version='1.4.96',
     description='Quantitative Trading Python Library',
     long_description=long_description,
     url='https://github.com/ranaroussi/qtpylib',
