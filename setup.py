67c8fdb49cf8514338e805a6ffb3c066979ff93c
ran <ran@aroussi.com>
2016-11-13 15:46:37 +0200

    updated version #

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='QTPyLib',
-    version='1.5.30',
+    version='1.5.31',
     description='Quantitative Trading Python Library',
     long_description=long_description,
     url='https://github.com/ranaroussi/qtpylib',
